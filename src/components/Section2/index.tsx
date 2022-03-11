4b5f3f246230d4a65b85a7ea2c12dcfeaa2265ae
Gabriel  Dias <gabrieldiascorrea7@gmail.com>
2022-03-11 12:56:32 -0300

    verification facebook

--- src/components/Section2/index.tsx
+++ src/components/Section2/index.tsx
@@ -7,7 +7,10 @@ interface ElementsSectionProps {
     data: ElementsSection2[]
 }
 
+
+
 export default function Section2({ data }: ElementsSectionProps) {
+
 
     return (
         <Container>
@@ -17,11 +20,24 @@ export default function Section2({ data }: ElementsSectionProps) {
                 {data.map((value, key) => (
                     <div key={key}>
                         <div className='video-area' >
-                            <ReactPlayer width="100%" height="100%" className="video" url={value.link_youtube} controls />
+                            <ReactPlayer width="100%" height="100%" url="http://www.youtube.com/embed/gLEYYR3V5GQ?autoplay=1&origin=http://localhost:3000"
+                                config={{
+                                    youtube: {
+                                        playerVars: {
+                                            'autoplay': 1,
+                                            'controls': 0,
+                                            'autohide': 1,
+                                            'wmode': 'opaque',
+                                            'origin': 'http://localhost:3000' 
+                                        }
+                                    }
+                                }}
+                                controls
+                            />
                         </div>
 
                         <div dangerouslySetInnerHTML={{ __html: value.title }} />
-                        
+
                         <div dangerouslySetInnerHTML={{ __html: value.text }} />
                     </div>
                 ))}
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -10,6 +10,7 @@ export default class MyDocument extends Document {
                     <link rel="preconnect" href="https://fonts.googleapis.com" />
                     <link rel="preconnect" href="https://fonts.gstatic.com" />
                     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;600;700&display=swap" rel="stylesheet" />
+                    <meta name="facebook-domain-verification" content="b28sgql9k4ojudz3hauhud27awynt5" />
                 </Head>
 
                 <body>
